1d0bb12c5fd5d081562f0fb49949adf4c285e29d
Marcin Ulezalka <36479486+marcinulezalka@users.noreply.github.com>
2026-01-31 20:14:19 +0100

    Add Polish language support for cookie consent
    Added Polish translations for cookie consent modals.

---
+++ lang/pl/cookies.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'consentModal' => [
+        'title' => 'Ciasteczka i Prywatność',
+        'description' => 'Używamy ciasteczek, aby poprawić komfort użytkowania.',
+        'acceptAllBtn' => 'Akceptuj wszystko',
+        'acceptNecessaryBtn' => 'Tylko niezbędne',
+        'showPreferencesBtn' => 'Ustawienia'
+    ],
+    'preferencesModal' => [
+        'title' => 'Ustawienia cookies',
+        'savePreferencesBtn' => 'Zapisz ustawienia',
+        'acceptAllBtn' => 'Akceptuj wszystko',
+        'sections' => [
+            ['title' => 'Niezbędne', 'description' => 'Wymagane do działania strony.', 'linkedCategory' => 'necessary']
+        ]
+    ]
+];
